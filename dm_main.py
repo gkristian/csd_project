f14e5467f342954c937f5a306f0fbf8c6913ac1b
angli <angli@kth.se>
2016-10-12 09:40:38 +0200

    more cleaning

--- dm_main.py
+++ dm_main.py
@@ -11,6 +11,9 @@ import os
 import sys
  
 try:
+
+	# TODO, structure to quit threads cleanly
+
 	time_intervall = 2 # set time between pushes to db
 	rpm_keys = []
 	hum_keys = []
@@ -42,28 +45,20 @@ try:
 		monitor_t.daemon = True
 		monitor_t.start()
 	except KeyboardInterrupt: # to exit
-		#sys.exit 
 		os._exit(0) # ugly but working way to kill threads after we interrupt the server
 	except BaseException as e:
 	    print e
 
-
-	# print "\nThe current view of  the nfm cache"
-	# cache.print_module_cache_items("nfm")
-
-	# print json.loads(cache.get_all_values())
-
-	# #  TODO ONLY FOR TESTING, TO BE REMOVED
-	# #PRETEND CONTROLLER REQUEST DATA HERE
-	# try:
-	# 	print "\nGet the specified subset of values from NFM:"
-	#     #returns a subset of  specified module values
-	# 	print cache.get_values(controller_data)
-	# 	print "Get success"
-	# except BaseException as e:
-	#     print e
-	# TODO ABOVE IS ONLY FOR TESTING, TO BE REMOVED, NORMAL CODE BELOW
-
+	try:
+		# start thread for listening to controller module
+		PORT = 8070
+		controller_t = threading.Thread(target=start_serving, args=(PORT, cache, "controller module"))
+		controller_t.daemon = True
+		controller_t.start()
+	except KeyboardInterrupt: # to exit
+		os._exit(0) # ugly but working way to kill threads after we interrupt the server
+	except BaseException as e:
+	    print e
 
 	# function to push from cache, used in our repeattimer
 	def push_from_cache():

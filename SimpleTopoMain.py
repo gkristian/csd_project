c4e687da52df2fc9e39fc5bf669a470dba862963
Joakim Sandberg <jockesandberg21@gmail.com>
2016-10-22 12:30:57 +0200

    CURRENT NFM WITH LU

--- SimpleTopoMain.py
+++ SimpleTopoMain.py
@@ -26,12 +26,14 @@ except:
 ip=sys.argv[1]
 port=sys.argv[2]
 
+#print(ip, port)
+
 
 
 #create the mininet object
 try:
-	net = Mininet( topo=SimpleTopo.SimpleTopo(), switch=OVSSwitch, link=OVSLink, controller=RemoteController('c0', ip=ip, port=int(port)))
-	print("SUCCESS")
+	net = Mininet( topo=SimpleTopo.SimpleTopo(), switch=OVSSwitch, link=TCLink, controller=RemoteController('c0', ip=ip, port=int(port)))
+	#print("SUCCESS")
 except:
 	print("NO NET")
 
@@ -40,47 +42,135 @@ net.start()
 
 
 
-#Set commands on switches to use OF v 1.3 and enable STP
+#Set commands on switches to use OF v 1.3 and adding a flow
 def command_switch(name, priority=None):
 	switch_obj = net.get(name)
 	try:
 		switch_obj.cmd('ovs-vsctl set Bridge '+name+' protocols=OpenFlow13')
-		print("Setting protocol correctly")
+		#print("Setting protocol correctly")
 	except:
 		print("Error setting protocol on switch:", name)
-	"""
+	
 	try:
-		if name=='s4':
-			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-#					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:04:01,actions=output:3')
-			
-			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-					' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
-			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-					' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
-			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-					' idle_timeout=180,priority=32000,in_port=1,actions=output:2')
-			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-					' idle_timeout=180,priority=32000,in_port=2,actions=output:1')
 		
-		if name=='s5':
+		if name=='s1':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:02:01,dl_dst=00:00:00:00:01:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:03:01,dl_dst=00:00:00:00:01:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:01:01,dl_dst=00:00:00:00:02:01,actions=output=1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:01:01,dl_dst=00:00:00:00:03:01,actions=output=2')
+		
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=32000,in_port=1,actions=output:2')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=32000,in_port=2,actions=output:1')
+				
+		if name=='s2':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:01:01,dl_dst=00:00:00:00:02:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:03:01,dl_dst=00:00:00:00:02:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:02:01,dl_dst=00:00:00:00:01:01,actions=output=1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:02:01,dl_dst=00:00:00:00:03:01,actions=output=2')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:01:01,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:07:01,actions=output:3')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=32000,in_port=1,actions=output:2')
+		
+		if name=='s3':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:01:01,dl_dst=00:00:00:00:03:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:02:01,dl_dst=00:00:00:00:03:01,actions=output=3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:03:01,dl_dst=00:00:00:00:01:01,actions=output=2')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_src=00:00:00:00:03:01,dl_dst=00:00:00:00:02:01,actions=output=1')
 			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
 			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:05:01,actions=output:3')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:04:01,actions=output:2')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:01:01,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:07:01,actions=output:4')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=2,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=31000,in_port=1,actions=output:4')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priotity=31000,in_port=4,actions=output:1')
+		"""
+		if name=='s4':
+			
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:04:01,actions=output:3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:01:01,actions=output:1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow ' +name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:05:01,actions=output:1')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=32000,in_port=1,actions=output:3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=32000,in_port=3,actions=output:1')
+			
+		if name=='s5':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:05:01,actions=output:3')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:01:01,actions=output:1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow ' +name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:04:01,actions=output:1')	
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=32000,in_port=1,actions=output:3')
 
 			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
-					' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
+					' idle_timeout=180,priority=32000,in_port=3,actions=output:1')
+		if name=='s6':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,in_port=1,actions=output:2')
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,in_port=2,actions=output:1')
+		
+		if name=='s7':
+			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+					' idle_timeout=180,priority=33000,dl_dst=00:00:00:00:07:01,actions=output:3')
+			
+			
+			#switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
+			#		' idle_timeout=180,priority=33000,in_port=1,actions=output:3')
 			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
 					' idle_timeout=180,priority=33000,in_port=3,actions=output:1')
 			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
 					' idle_timeout=180,priority=32000,in_port=1,actions=output:2')
-
 			switch_obj.cmd('ovs-ofctl -O Openflow13 add-flow '+name+
 					' idle_timeout=180,priority=32000,in_port=2,actions=output:1')
-
-			
+		"""
 	except:
 		print("Error adding pre-flows")
 	
+	"""
 	try:	
 		switch_obj.cmd('ovs-vsctl set Bridge', name, 'stp_enable=true', 'other_config:stp-priority=%d' % priority)
 		print("Setting stp correctly")
@@ -102,9 +192,10 @@ command_switch('s1')
 #prio += 1
 command_switch('s2')
 command_switch('s3')
-command_switch('s4')
-command_switch('s5')
-command_switch('s6')
+#command_switch('s4')
+#command_switch('s5')
+#command_switch('s6')
+#command_switch('s7')
 
 CLI(net)
 net.stop()

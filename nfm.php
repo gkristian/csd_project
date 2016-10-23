0ab6846f9f8899be5bd6f8042015bc1466153745
gkristianp <gkristianp@gmail.com>
2016-10-23 11:58:05 +0200

    Modify PHP to comply with newest data structure. Raw NFM data can be shown, but no further processing to separate between links

--- nfm.php
+++ nfm.php
@@ -1,36 +1,88 @@
-//Main file to visualize NFM table and graph
 <?php
-require_once ('connection.php');
+//PHP file to visualize NFM table and graph
+//Purwidi 2016
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+//Connection establishment
+require('connection.php');
+
+//Get all nfm data
 $q1 = "SELECT * FROM nfm";
-$result = $con	->query($q1);
+if ($res1 = $con->query($q1)) {
+    printf("NFM table fetched<br>");
+} else {
+    printf("No data from NFM table<br>");
+}
 
-	if ($result->num_rows > 0) {
-?>
-		<html>
-			<h1>NFM Data</h1>
-			<table border=1>
-			<tr>
-				<th>id</th>
-				<th>flow</th>
-				<th>delay</th>
-			</tr>
+//Get NFM's number of keys (#links)
+$q2 = "SELECT count FROM keycount WHERE module='nfm'";
+if ($res2 = $con->query($q2)) {
+	printf("NFM keycount fetched. #links :");
+	$row2 = $res2->fetch_assoc();
+	$nfmkeycount = intval($row2['count']); //Convert result string to int
+	echo $nfmkeycount . gettype($nfmkeycount);
+} else {
+	printf("No data from keycount<br>");
+}
 
-<?php
-		// output data of each row
-    	while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row['id'] . "</td>";
-			echo "<td>" . $row['flow'] . "</td>";
-			echo "<td>" . $row['delay'] . "</td>";
-			echo "</tr>";
-    	}
-	} else {
-    	echo "0 results";
+//Get link names
+$q3 = "SELECT link FROM nfm LIMIT $nfmkeycount";
+if ($res3 = $con->query($q3)) {
+    printf("<br>Link names fetched");
+} else {
+    printf("<br>Failed to fetch link names<br>");
+}
+
+echo "<html>";
+//Make 3 big columns for 2 data and graph
+echo "<table><tr><td>";
+
+//Construct RAW NFM table=================================
+if ($res3->num_rows > 0) {
+	echo "<h2>RAW NFM Table</h2>";
+	//HEADER
+	echo "<table border=1><tr><th>timestamp</th><th>link</th><th>utilization</th></tr>";
+    while($row1 = $res1->fetch_assoc()) {//Data for each row
+		echo "<tr>";
+		echo "<td>" . $row1['timestamp'] . "</td>";
+		echo "<td>" . $row1['link'] . "</td>";
+		echo "<td>" . $row1['util'] . "</td>";
+		echo "</tr>";
 	}
-	//DON'T FORGET TO CLOSE
-	$con->close();
-?>
-</table>
+	echo "</table>";
+} else {
+   	echo "Error : Construct table";
+}
 
-<img src="plotnfm.php"> 
-</html>
+echo "</td><td valign='top'>";
+//Construct table of link utilization=====================
+echo "<h2>Processed NFM table</h2>";
+
+if ($res3->num_rows > 0) {
+    echo "<table border=1><tr><th>timestamp</th>"; //HEADER
+    while($row3 = $res3->fetch_assoc()) {
+        echo "<th>" .$row3['link']. "</th>";
+    }
+    echo "</tr>";/*
+    while($row1 = $res1->fetch_assoc()) {//Data for each row
+        echo "<tr>";
+        echo "<td>" . $row1['timestamp'] . "</td>";
+        echo "<td>" . $row1['link'] . "</td>";
+        echo "<td>" . $row1['util'] . "</td>";
+        echo "</tr>";
+    }*/
+	echo "</table>";
+} else {
+    echo "Error : Construct table";
+}
+
+
+echo "</td><td valign='top'>";
+//Data visualization====================================
+echo "<h2>Graphs</h2>";
+echo "<img src='plotnfm.php'>";
+
+//End of main table
+echo "</td></tr></table></html>";
+$con->close(); //DON'T FORGET TO CLOSE
+?>

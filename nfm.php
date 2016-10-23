310f4e385b8ddbc4473169a9ca5cc183619e601a
gkristianp <gkristianp@gmail.com>
2016-10-23 23:49:03 +0200

    Now can visualize data in graph.

--- nfm.php
+++ nfm.php
@@ -1,8 +1,10 @@
 <?php
 //PHP file to visualize NFM table and graph
 //Purwidi 2016
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 //Connection establishment
 require('connection.php');
 
@@ -35,7 +37,7 @@ if ($res3 = $con->query($q3)) {
 
 echo "<html>";
 //Make 3 big columns for 2 data and graph
-echo "<table><tr><td>";
+echo "<table><tr><td valign='top'>";
 
 //Construct RAW NFM table=================================
 if ($res3->num_rows > 0) {
@@ -56,14 +58,14 @@ if ($res3->num_rows > 0) {
 
 echo "</td><td valign='top'>";
 //Show  table of link utilization for each link=====================
-echo "<h2>Processed NFM table</h2>";
+echo "<h2>Processed table</h2>";
 if ($res3->num_rows > 0) {
 	$i=0;
-	//$linklist=array('apple','banana','ha','hi');
 	$arrall=array(); //Contains the whole NFM table
 	$arrlinkname=array();
     echo "<table border=1><tr><th>timestamp</th>"; //HEADER
 
+	//Fetch link names
 	while($row3 = $res3->fetch_assoc()) {
 		array_push($arrlinkname,$row3['link']);
         echo "<th>" .$arrlinkname[$i]. "</th>";
@@ -71,47 +73,42 @@ if ($res3->num_rows > 0) {
     }
     echo "</tr><br>";
 
-	/*Create array for each link. Work as follow :
+	/*Create array cointaining data for every link. Work as follow :
 	1. Get data for each link from raw nfm table
 	2. Assign data to correct structure in $arrall. Try print_r to understand
+	$arrall[$link][$timestamp][$column] -> column means time and utilization
 	*/
+	$linkindex=0;
 	foreach ($arrlinkname as &$currentlink) {
-		echo "hi-";
-		$j=0;
-		if ($res4 = $con->query("SELECT * FROM nfm WHERE link=$currentlink")) {
+		$timeindex=0;
+		if ($res4 = $con->query("SELECT * FROM nfm WHERE link='$currentlink'")) {
 		    while($row4 = $res4->fetch_assoc()) {//Data for each row
-				$arrall[$currentlink][$j]=array(
+				$arrall[$linkindex][$timeindex]=array(
 					$row4['timestamp'],
 					$row4['util']
 				);
-				$j=$j+1;
+				$timeindex=$timeindex+1;
 			}
 		} else {
 			echo "Error xx";
 		}
+		$linkindex=$linkindex+1;
 	}
-	echo "<br>=================<br>";
-	echo "<br><br>ARRALL DUMP<br>";
-	print_r($arrall);
-/*	$arrall = array_flip($arrall);
-	echo "<br>AFTER FLIP";
-	foreach ($arrall as &$value) {
-	    echo "<br>LINKNAME" . $value;
+	//print_r($arrall);
+	$linkcount = count($arrall);
+	$timecount = count($arrall[0]);
+	echo "<br>#timestamp : " . $timecount . gettype($timecount);
+	echo "<br>#links : " . $linkcount . gettype($linkcount);
 
+	//Create rows for data
+	for($time=0;$time<$timecount;$time++){
+		echo "<tr>";
+		echo "<td>" . $arrall[0][$time][0] ."</td>";
+		for($link=0;$link<$linkcount;$link++){
+			echo "<td>" . $arrall[$link][$time][1] ."</td>";
+		}
+		echo "</tr>";
 	}
-	$arrall = array_flip($arrall);
-
-//	echo $arrlink['1-1'];
-
-/*
-    while($row1 = $res1->fetch_assoc()) {//Data for each row
-        echo "<tr>";
-        echo "<td>" . $row1['timestamp'] . "</td>";
-        echo "<td>" . $row1['link'] . "</td>";
-        echo "<td>" . $row1['util'] . "</td>";
-        echo "</tr>";
-    }
-	*/
 	echo "</table>";
 } else {
     echo "Error : Construct table";
@@ -121,7 +118,15 @@ if ($res3->num_rows > 0) {
 echo "</td><td valign='top'>";
 //Data visualization====================================
 echo "<h2>Graphs</h2>";
-echo "<img src='plotnfm.php'>";
+$data=array();
+for($link=0;$link<$linkcount;$link++){
+	$data=array();
+	for($time=0;$time<$timecount;$time++){
+		array_push($data,$arrall[$link][$time][1]);
+	}
+	$imploded=implode(":", $data);
+	echo "<img src=plotnfm.php?title=".$arrlinkname[$link]."&data=".urlencode($imploded).">";
+}
 
 //End of main table
 echo "</td></tr></table></html>";
--- plotnfm.php
+++ plotnfm.php
@@ -1,24 +1,23 @@
 <?php // content="text/plain; charset=utf-8"
+//Library
 require_once('jpgraph/src/jpgraph.php');
 require_once('jpgraph/src/jpgraph_line.php');
-require_once ('connection.php');
 
-$x_axis = array();
-$y_axis = array();
-$i = 0;
-
-$result = mysqli_query($con,"SELECT * FROM nfm"); 
-while($row = mysqli_fetch_array($result)) {
-	$x_axis[$i] = $row["id"];
-	$y_axis[$i] = $row["delay"];
-    $i++;
+//Receive data via GET
+$var_value = $_GET['data'];
+$title=$_GET['title'];
+$b = explode(":",urldecode($var_value));
+foreach ($b as &$value) {
+    $value = floatval($value);
 }
-mysqli_close($con);
+$y_axis = $b;
 
 // Create the graph. These two calls are always required
 $graph = new Graph(350,250);
 $graph->SetScale('textlin');
-$graph->xaxis->SetTickLabels($x_axis);
+$graph->title->Set("Link ".$title);
+$graph->title->SetFont(FF_DV_SANSSERIF, FS_BOLD, 14);
+//$graph->xaxis->SetTickLabels($x_axis);
 
 // Create the linear plot
 $lineplot=new LinePlot($y_axis);

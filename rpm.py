84176224354cfff54fe3c8a573b7c95a53e07ea9
angli <angli@kth.se>
2016-11-07 15:05:54 +0100

    Clean up

--- rpm.py
+++ rpm.py
@@ -25,9 +25,12 @@ class RPM(app_manager.RyuApp):
 	"""
 	def __init__(self, *args, **kwargs):
 		super(RPM, self).__init__(*args, **kwargs)
-		self.datapaths = {}
+		# used by RyuApp
+		self.datapaths = {} 
 
 		#self.net = app_manager._CONTEXTS['network']	#fetch graph object of physical network
+
+		# TODO HARDCODED, should be recived from controller by CONTEXT or events.
 		self.net = nx.DiGraph([(1,2,{'src_port':1,'dst_port':1,'src_dpid':1,'dst_dpid':2,'bw':10}),
 								(1,3,{'src_port':2,'dst_port':2,'src_dpid':1,'dst_dpid':3,'bw':30}),
 								(2,3,{'src_port':2,'dst_port':1,'src_dpid':2,'dst_dpid':3,'bw':20}),
@@ -46,28 +49,42 @@ class RPM(app_manager.RyuApp):
 		
 		self.logger.debug("TOTAL SWITCHES: %d", self.totalSwitchesNr)
 
+		# TODO communication with DM
 		self.DICT_TO_DB = {'module':'rpm'}	#prepare a dictionary for updating and sending to Database
 		
-		self.updateTime = 10
-		self.switches_DPIDs = {}	#dict to store all switches in order of dpid
+		self.switches_DPIDs = {}	#dict to store all datapath ojects by key dpid
 		
 		self.start_time = 0
 
 		# format {"dpid1": {"start_time": 100, "measured_time": 13, "xid": 1234}}
 		self.switches_data = {}
-		# format on position dpid, current xid
 
 	"""
 	Main loop of sending  install/update/delete flow mods to the switches,
-	currently sending ca 16 request every second. i.e get results for 4 switches every second
-	TODO Send to DM every 5-10 seconds?
+	currently max sending ca 32 request every second. i.e send request to a switch every nr_of_switches/32 seconds, 
+	any faster sending rate exceeds the serving rate of the event handler and builds up the quequing times.
+
+	TODO Send updates to DM
 	"""
 	def _monitor(self): #TODO CHANGE FOR NEW STRUCTURE
 		xid = 0
+		# set of switches
+		dpids = self.switches_DPIDs.viewkeys()
+		# define waiting time before sending next barrier request
+		sleeping = 0.25
+
+		# Current minimum waiting time, lower than this means that the request 
+		# sending rate exceeds the event handler serving rate and we get event 
+		# queque build up, which fucks up the measuring
+		min_sleeping = 0.03125
+
+		# TODO minimum waiting time may change with topology changes, investigate.
+
+		# Sending loop
 		while True:			
 			self._print("SENDING FLOW MODS...")
-			dpids = self.switches_DPIDs.viewkeys()
-			for dpid in dpids: # Testing flow mod + flow remove
+			for dpid in dpids: 
+				# Get the datapath object
 				dp = self.switches_DPIDs[dpid]
 
 				self.send_flow_mod(dp, 1, '00:00:00:00:00:00', "ADD")
@@ -77,31 +94,34 @@ class RPM(app_manager.RyuApp):
 				self.send_flow_mod(dp, 2, '20:00:00:00:00:00', "DELETE")
 				self._print("FLOW MODS SENT")
 
-				#TODO work in structure for saving away data
+				# set start time for measurment
 				self.switches_data[dpid]["start_time"] = int(round(time.time() * 1000))
+				# set xid to be able to identify the response
 				xid += 1
 				self.switches_data[dpid]["xid"] = xid
+				# send the barrier request
 				self.send_barrier_request(dp, xid)
 				self._print("BARRIER REQ WITH ID %d SENT" %xid)
 				
-				self._print(str(self.switches_data.viewitems()))
-				time.sleep(0.0625)
+				self._print("Data before barrier req: " + str(self.switches_data.viewitems()))
+				
+				# wait to send new monitoring requests
+				time.sleep(sleeping)
 
 
 
 	"""
-	Install or update a flow rule for a given switch/datapath. 
+	Install, update or delete a flow rule for a given switch/datapath, 
+	given in port number and ethernet destination address
 	"""
 	def send_flow_mod(self, datapath, in_port_nr, eth_dst_adr, command_dir):
 		ofp = datapath.ofproto
-		#self._print(ofp)
-
 		ofp_parser = datapath.ofproto_parser
 
 		cookie = cookie_mask = 0
 		table_id = 0
 		idle_timeout = 0
-		hard_timeout = 10
+		hard_timeout = 0
 		priority = 32768
 		buffer_id = ofp.OFP_NO_BUFFER
 
@@ -118,10 +138,10 @@ class RPM(app_manager.RyuApp):
 
 
 	
-		match = ofp_parser.OFPMatch(in_port=in_port_nr, eth_dst= eth_dst_adr)  # DUMMY IP
-		actions = [ofp_parser.OFPActionOutput(ofp.OFPP_NORMAL, 0)]
-		inst = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,actions)]
-		req = ofp_parser.OFPFlowMod(datapath, cookie, cookie_mask,
+		match = ofp_parser.OFPMatch(in_port=in_port_nr, eth_dst= eth_dst_adr)  		# What does flows does the rule capture, flows should be dummies
+		actions = [ofp_parser.OFPActionOutput(ofp.OFPP_NORMAL, 0)]					# What action should be taken, normal not flood
+		inst = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,actions)]	# set options and compose request
+		req = ofp_parser.OFPFlowMod(datapath, cookie, cookie_mask,				
 									table_id, command,
 									idle_timeout, hard_timeout,
 									priority, buffer_id,
@@ -130,9 +150,11 @@ class RPM(app_manager.RyuApp):
 									match, inst)
 		datapath.send_msg(req)
 
+	"""
+	Given a datapath object and an xid number send a barrier request
+	"""
 	def send_barrier_request(self, datapath, xid):
 		ofp_parser = datapath.ofproto_parser
-
 		req = ofp_parser.OFPBarrierRequest(datapath)
 		req.set_xid(xid)
 		datapath.send_msg(req)
@@ -144,10 +166,16 @@ class RPM(app_manager.RyuApp):
 	"""
 	def determineNumberOfSwitches(self):
 		return len([n for n in self.net.nodes() if len(str(n).split(':')) == 1])
-
+	"""
+	Print message with header RPM
+	"""
 	def _print(self, message):
 		self.logger.info("[RPM] " + message)
 
+	"""
+	Event handler for the event OFPFlowRemoved, 
+	ie. this event is triggered when a flow is removed by timeouts or delete commands
+	"""
 	@set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
 	def flow_removed_handler(self, ev):
 		self._print("FLOW REMOVED")
@@ -156,8 +184,6 @@ class RPM(app_manager.RyuApp):
 		dp = msg.datapath
 		ofp = dp.ofproto
 		ofp_id = dp.id
-
-		#TODO BE ABLE TO IDENTIFY RPM FLOW EVENT
 
 		self._print("Switch with id: %d" % ofp_id)
 
@@ -172,7 +198,7 @@ class RPM(app_manager.RyuApp):
 		else:
 			reason = 'unknown'
 
-		self._print(reason)
+		#self._print(reason)
 
 
 		self.logger.debug('OFPFlowRemoved received: '
@@ -185,24 +211,38 @@ class RPM(app_manager.RyuApp):
 						msg.idle_timeout, msg.hard_timeout,
 						msg.packet_count, msg.byte_count, msg.match)
 	
-	
+	"""
+	Event handler for the event OFPBarrierReply, 
+	ie. this event is triggered when a barrier reply is sent after 
+	a barrier request have forced the switch to install all quequed rules.
+	"""
 	@set_ev_cls(ofp_event.EventOFPBarrierReply, MAIN_DISPATCHER)
 	def _handle_barrier(self, ev):
-		msg = ev.msg
-		datapath = msg.datapath
-		dpid = datapath.id
-		self._print("Barrier datapath %d" % dpid)
-
-		current_time = int(round(time.time() * 1000))
-		starting_time = self.switches_data[dpid]["start_time"]
-		timed = current_time - starting_time
-
-		self.switches_data[dpid]["measured_time"] = timed
-		
 		self._print('OFPBarrierReply received')
-		# Time taken for installation of all current waiting rules and RTT
-		print "Timer from barrier req to resp: %d (ms) for datapath %d" % (timed, datapath.id)
+		
+		msg = ev.msg
+		xid = msg.xid
+		datapath = msg.datapath
+		# DPID from sending switch
+		dpid = datapath.id
+		# Last sent messages xid
+		req_xid = self.switches_data[dpid]["xid"]
 
+		if req_xid == xid:
+			current_time = int(round(time.time() * 1000)) 			# current time in milliseconds
+			starting_time = self.switches_data[dpid]["start_time"] 	# also in milliseconds
+			timed = current_time - starting_time					# measured time between barrier request and response
+			# Store the measured time
+			self.switches_data[dpid]["measured_time"] = timed
+		
+			# Time taken for installation of all current waiting rules and RTT 
+			self._print("Timer from barrier req to resp: %d (ms) for datapath %d" % (timed, datapath.id)) # why is this longer for a slower sending rate?!
+			self._print("Request xid %d, reply xid %d" % (self.switches_data[dpid]["xid"], xid))
+		else:
+			if xid > req_xid:
+				self._print("Sending rate exceeds handling rate")
+			else:
+				self._print("?!? Something weird is going on")
 
 	"""
 	Switch state change handler. Called every time a switch state changes, creates a dict dpid -> datapath

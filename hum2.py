6c88ee9e67d33a8f15b9bf342e072a1b14d1555f
hamzehm <hamzehm@kth.se>
2016-11-26 00:31:01 +0100

    HUM: memory and core usage

---
+++ hum2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import psutil, os, sys, time
+from subprocess import check_output
+
+def core_usage():
+    cpu_perc = psutil.cpu_percent(interval=0, percpu=True)
+    for i in range(len(cpu_perc)):
+        print "-"*15
+        print "Core Utilization"
+        print "Usage of Core", str(i+1),":", str(cpu_perc[i]), "%"
+        print "-"*15
+def mem_usage():
+    memory=psutil.virtual_memory()
+    memory_usage=memory.percent
+    print "-"*15
+    print "Memory Utilization"
+    print "Memory:  ",memory_usage, "% "
+    print "-"*15
+
+
+while True:
+    time.sleep(5)
+    core_usage()
+    mem_usage()
+
+
+
+#for i in range(10):
+#    time.sleep(5)
+#    core_usage()
+#    mem_usage()
+
+
+print "-"*15

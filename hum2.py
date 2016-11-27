23fb42f922f78d5f578d6928ff53fb7e94a471e1
gkristianp <gkristianp@gmail.com>
2016-11-27 23:15:53 +0100

    Added REST functionality to HUM

--- hum2.py
+++ hum2.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python
 import psutil, os, sys, time
+from client import client_side
+from datetime import datetime
 from subprocess import check_output
 
+url = 'http://127.0.0.1:8000/Tasks.txt'
+DICT_TO_DB = {'module':'hum', 'timestamp': -1, 'core':{},'memory':-1}
+
 def core_usage():
-    cpu_perc = psutil.cpu_percent(interval=0, percpu=True)
-    for i in range(len(cpu_perc)):
-        print "-"*15
-        print "Core Utilization"
-        print "Usage of Core", str(i+1),":", str(cpu_perc[i]), "%"
-        print "-"*15
+	cpu_perc = psutil.cpu_percent(interval=0, percpu=True)
+	for i in range(len(cpu_perc)):
+		print "Usage of Core", str(i+1),":", str(cpu_perc[i]), "%"
+	return 
+
 def mem_usage():
-    memory=psutil.virtual_memory()
-    memory_usage=memory.percent
-    print "-"*15
-    print "Memory Utilization"
-    print "Memory:  ",memory_usage, "% "
-    print "-"*15
+	memory=psutil.virtual_memory()
+	memory_usage=memory.percent
+	print "Memory Utilization"
+	print "Memory:  ",memory_usage, "% "
 
+def timestamp():
+	timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+	print timestamp
 
+client = client_side(url)
 while True:
-    time.sleep(5)
-    core_usage()
-    mem_usage()
-
+	timestamp()
+	core_usage()
+	mem_usage()
+	client.postme(DICT_TO_DB)
+	time.sleep(1)

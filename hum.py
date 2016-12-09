8847a69f5f7585deb465d4e9c59363eaa54ba8d2
gkristianp <gkristianp@gmail.com>
2016-12-09 06:46:01 +0100

    Manual calculation of /proc/stat nearly finished

--- hum.py
+++ hum.py
@@ -16,20 +16,55 @@ def timestamp():
 	#return timestamp
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
+def core_manual():
+	#return core usage measurement by manually calculating value in /proc/stat
+	#reference :http://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux?noredirect=1&lq=1
+	previdle = 0
+	prevnonidle = 0
+	prevtotal = 0
+	with open('/proc/stat','r') as f:
+		#print "opened"
+		key1="cpu"
+		n=0
+		for i,line in enumerate(f):
+			key="".join((key1,str(n)))
+			#get only the line with cpu[i]
+			if key in line:
+				valuestr = str(line).split()		#split one line to list. delimiter : space
+				del valuestr[0]						#delete the name cpu[n]
+				val = [int(x) for x in valuestr]	#integer conversion
+				#print val
+				#calculation of cpu usage in percent
+				idle = val[3] + val[4]
+				nonidle = val[0]+val[1]+val[2]+val[5]+val[6]+val[7]
+				total = idle + nonidle
+				totald = total - prevtotal
+				idled = idle - previdle
+				cpu_percent = (totald-idled)/totald
+				print cpu_percent
+				previdle = idle
+				prevnonidle = nonidle
+				prevtotal = total
+				n = n+1
+
 def core_mpstat():
 	#return core usage measurement (percent) by mpstat
 	#Calculated by substracting 100 with %idle (%usage=100-%idle) to get every type of cpu usage (user,kernel,irq,etc)
+	dict_core = {}
 	os.system("mpstat -P ALL > mpstat.log")
 	with open('mpstat.log','r') as mpstatlog:
+		n=0
 		for i, line in enumerate(mpstatlog):
 			if i > 3:
 				#cpu data start at line 4
 				res=line[-7:] 					#read last 7 char (%idle)
 				res = res[:3] + '.' + res[4:] 	#change comma to dot
 				res = res.strip()				#remove space and \n
-				cpu_usage = 100- float(res)
-				print cpu_usage
-
+				cpu_usage = 100-float(res)
+				#print cpu_usage
+				dict_core[n]=str(cpu_usage)
+				n=n+1
+	return dict_core
 
 def core_psutil():
 	#return core usage measurement (percent) by psutil
@@ -50,10 +85,10 @@ def mem_psutil():
 print "|CPU:psutil	|CPU:mpstat	|MEM:psutil	|MEM:mpstat	|"
 
 while True:
-	core_mpstat()
+	core_manual()
 	#Insert all data to dictionary.
 	DICT_TO_DB['timestamp']=timestamp()
-	DICT_TO_DB['core']=core_psutil()
+	DICT_TO_DB['core']=core_mpstat()
 	DICT_TO_DB['memory']=mem_psutil()
 	#print DICT_TO_DB
 	#client.postme(DICT_TO_DB)

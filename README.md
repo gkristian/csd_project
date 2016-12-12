1293450196beefefbb50f617529b5cfc75e055ea
Adnan Shafi <ashafi@kth.se>
2016-12-12 04:57:46 +0100

    This one works, but not with latest NFM but the one before

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# monitoring_SDN
+#To run the project
+make startall
+#To stop the project
+make stopall
+#To check process status
+make show
+#To look at the logs
+make dmlog
+make mlog
--- test/rest_test_nfm.py
+++ test/rest_test_nfm.py
@@ -9,6 +9,9 @@ del path
 
 from client import client_side
 
+
+def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+
 url = 'http://127.0.0.1:8000/Tasks.txt'
 DMclient = client_side(url)
 """
@@ -23,7 +26,8 @@ if currentMillis - self.lastTimeRequest > 10000:
 """
 
 #response = self.DMclient.getme({'module':'nfm', 'timestamp':0,'keylist':['link_utilization']})
-dict1={'module':'nfm', 'timestamp':0,'keylist':['link_utilization']}
+####dict1={'module':'nfm', 'timestamp':0,'keylist':['link_utilization']}
+dict1={'module':'nfm', 'keylist':['link_utilization']}
 
 #RPM dict
 #rpmdict = {'module':'rpm', 'timestamp': -1, 'delays':{}, 'max_delay': -1, 'min_delay': -1, 'mean_delay': -1}
@@ -42,6 +46,50 @@ response = DMclient.getme(dict1)
 #######response = DMclient.getme(rest_query_dict_nfm)
 print "request is"
 
+"""
+Output I got was:
+request is
+{'keylist': ['link_utilization'], 'module': 'nfm'}
+And response is
+[[u'link_utilization', {u'2-1': 0.0, u'1-2': 0.0}]]
+
+"""
+
 print  dict1
 print "And response is "
 print response
+
+response1 = response[0]
+graph = response1[1]
+del response1
+del response
+
+print 'graph = ',graph
+#tthe output in log was : 
+#graph =  {u'2-1': 0.0, u'1-2': 0.0}
+
+for gkey in graph:
+	gkey = gkey.encode('ascii','ignore')
+	print gkey, 'corresponds to', graph[gkey]
+	src_dpid,dst_dpid =gkey.split('-') #returns a lista
+	src_dpid = src_dpid.strip() #default to removing white spaces 
+	dst_dpid = dst_dpid.strip() #defaults to removing white spaces
+	
+	value = clamp(graph[gkey],0,100) #the link_utilization value must be between 0 to 100
+	#value = clamp(-300.0023,0,100) #the link_utilization value must be between 0 to 100
+	#value2 = clamp(300.0023,0,100) #the link_utilization value must be between 0 to 100
+	
+	print src_dpid,'-',dst_dpid,'====',value
+	
+
+	#self.net.edge[src_dpid][dst_dpid]['link_utilization'] = graph[gkey]
+	
+
+	
+
+	
+	
+	
+	
+	
+	

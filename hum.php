334748a8f2142cd58ff4435d5daf3bd87f8ff81a
gkristianp <gkristianp@gmail.com>
2017-01-02 15:06:34 +0100

    Clean up hum.php

--- hum.php
+++ hum.php
@@ -18,36 +18,58 @@ echo "<html>";
 echo "<meta http-equiv='refresh' content='10'>";
 echo "<b>Debug Message :</b><br>";
 
-//Get all hum data
+//Get all hum data and save them into array=============================
+$data = array();
 $q1 = "SELECT * FROM hum";
 if ($all = $con->query($q1)) {
     printf("HUM table fetched<br>");
+    while($row1 = $all->fetch_assoc()) {
+        array_push($data,$row1);
+    }
 } else {
     printf("No data from HUM table<br>");
 }
+//Convert cpu usage data from json format to php array
+for ($j=0;$j<19;$j++) {
+    $data[$j]['core']=json_decode($data[$j]['core'],True);
+}
+//Take the latest 19 data
+$sliced = array_slice($data, -19);
+//Get number of cpu
+$cpucount=count($data[0]['core']);
 
-//SECTION FOR MEMORY GRAPHS==============================================$
+//SECTION FOR MEMORY GRAPHS=============================================
+$arraymem=array();
+for ($i=0;$i<19;$i++) {
+    array_push($arraymem,$sliced[$i]['memory']);
+}
+$imploded=implode(":",$arraymem);
 echo "<center>";
 echo "<font face='calibri' size='6'><b>HUM Graphs</font><br>";
 echo "data for the past 3 minutes</b><br>";
-$data=array();
-while($row1 = $all->fetch_assoc()) {
-    array_push($data,$row1['memory']);
-}
-$tobesend = array_slice($data, -19); //Take the latest 20 data
-$imploded=implode(":",$tobesend);
 echo "<img src=humplotmem.php?data=".urlencode($imploded).">";
 echo "<br><br>";
 
-//SECTION FOR RAW HUM table====================================================$
-echo "<h2>RAW HUM Table</h2>";
-//HEADER
-echo "<table border=1><tr><th>timestamp</th><th>core</th><th>memory</th>";
-while($row2 = $all->fetch_assoc()) {//Data for each row
-	echo "<tr>";
-    echo "<td>" . $row1['timestamp'] . "</td>";
-    echo "<td>" . $row1['core'] . "</td>";
-    echo "<td>" . $row1['memory'] . "</td>";
+//SECTION FOR CPU GRAPHS===============================================
+
+
+//SECTION FOR RAW HUM table=============================================
+echo "<h2>CPU and Memory Usage Table</h2>";
+//Tale header
+echo "<table border=1><tr>";
+echo "<th width='250px'>Timestamp</th>";
+echo "<th width='90px'>Memory(%)</th>";
+for ($i=0;$i<$cpucount;$i++){
+    echo "<th width='80px'>Core " . $i . " (%)</th>";
+}
+//Table body
+for ($k=0;$k<count($data);$k++){
+    echo "<tr>";
+    echo "<td>" . $data[$k]['timestamp'] . "</td>";
+    echo "<td>" . $data[$k]['memory'] . "</td>";
+    for ($l=0;$l<count($data[0]['core']);$l++){
+        echo "<td>" . $data[$k]['core'][$l] . "</td>";
+    }
     echo "</tr>";
 }
 echo "</table>";

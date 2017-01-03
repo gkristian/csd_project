58bafa4f4af3934a54d15bea50c2192a4e8c3238
Adnan Shafi <ashafi@kth.se>
2017-01-03 09:51:52 +0000

    Testing with NFM

--- test_integrate_run2.sh
+++ test_integrate_run2.sh
@@ -1,2 +1,2 @@
- ryu-manager  --ofp-tcp-listen-port 6633 --observe-links --install-lldp-flow --default-log-level 3  --log-file /var/www/html/spacey/csd.log ./netflowmodule-not-latest-but-works.py ../controller_core/src/controller_core_of13.py
+ ryu-manager  --ofp-tcp-listen-port 6633 --observe-links --install-lldp-flow --default-log-level 3  --log-file /var/www/html/spacey/csd.log ../NFM/netflowmodule-not-latest-but-works-updated.py ../controller_core/src/controller_core_of13.py
 
